acc0fe71c2ecfa3171bb38693bd07676eed8a2a7
mohamuhsin <muhsinohoho@outlook.com>
2024-11-24 04:38:56 +0300

    added new meetup form

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,8 @@
+import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+
+export default function NewMeetup() {
+    function addMeetupHandler(enteredMeetupData) {
+        console.log(enteredMeetupData);
+    }
+    return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
