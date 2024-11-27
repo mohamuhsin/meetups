6a1f77a0bc6243f368d3c2d449365c0b8557777c
mohamuhsin <muhsinohoho@outlook.com>
2024-11-27 21:15:55 +0300

    added custom component and css modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+export default function MeetupDetailPage() {
+    return (
+        <MeetupDetail
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbIrVIBbLadPLgwGLL7VPryCRPONecVhYDLQ&s"
+            title="A First Meetup"
+            address="Some address, 5, 12345 some city"
+            description="A first meetup"
+        />
+    );
+}
